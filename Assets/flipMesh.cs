3be32d7ab4d041af40960bb67945f0696ba05ca7
Elliot Plant <elliotaplant@gmail.com>
2016-09-17 18:35:39 -0700

    Create hallway with inner material

---
+++ Assets/flipMesh.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class flipMesh : MonoBehaviour {
+
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.triangles = mesh.triangles.Reverse().ToArray();
+	}
+}
+
+
