9efbcf4865fdff5cf76ab975e568777df1253923
Elliot Plant <elliotaplant@gmail.com>
2016-09-17 21:26:54 -0700

    Create elastic bouncy ball

---
+++ Assets/Scripts/Bouncy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouncy : MonoBehaviour {
+	
+	public float bounciness;
+	private Vector3 lastVelocity;
+	private Rigidbody rb;
+
+	public void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		bounciness = Mathf.Clamp(bounciness, 0.0f, 1.0f);
+	}
+
+	void FixedUpdate()
+	{
+		if (rb) {
+			lastVelocity = rb.velocity;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Vector3 normal = Vector3.zero;
+
+		foreach(ContactPoint c in collision.contacts)
+		{
+			normal += c.normal;
+		}
+
+		normal.Normalize();
+		Vector3 inVelocity = lastVelocity;
+		Vector3 outVelocity = bounciness * ( -2.0f * (Vector3.Dot(inVelocity,normal) * normal) + inVelocity );
+		rb.velocity = outVelocity;
+	}
+}
---
+++ Assets/Scripts/StartVelocity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartVelocity : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = Random.insideUnitSphere * speed; //new Vector3(1.0f, 1.0f, 1.0f) * speed;
+	}
+}
