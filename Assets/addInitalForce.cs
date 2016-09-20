c57ac1fbcfd04391ea57f4cd61b13fd1ed12b8ce
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-09-19 23:34:16 -0700

    Create curveball scene with native physics

---
+++ Assets/addInitalForce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addInitalForce : MonoBehaviour {
+
+	// Use this for initialization
+	public float thrust;
+	public Rigidbody rb;
+	private Vector3 pos;
+	private Vector3 vel;
+	private Vector3 spin;
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce (2, 4, 10, ForceMode.Impulse);
+		rb.angularVelocity = new Vector3 (0f, 0.6f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = transform.position;
+		vel = rb.velocity;
+		spin = rb.angularVelocity;
+//		rb.AddForce (-pos.x * 4f, (-pos.y + 5f) * 4f, 0f);
+//		Debug.Log(Vector3.Cross(spin, vel));
+		rb.AddForce (Vector3.Cross(vel, spin));
+	}
+}
+	
