abdb3b83a700574b355ab3ab6c4ecb6eca76ab22
Elliot Plant <elliotaplant@gmail.com>
2016-10-02 13:31:08 -0700

    Make enemy ai

---
+++ Assets/Scripts/ballFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballFollow : MonoBehaviour {
+
+	public GameObject ballToFollow;
+	public float speed;
+	private Vector3 paddlePosition;
+	private Vector3 ballPosition;
+	
+	// Update is called once per frame
+	void Update () {
+		// get ball position
+		ballPosition = ballToFollow.transform.position;
+		// get vector between paddle and ball
+		paddlePosition = transform.position;
+		Vector3 paddleDirection = ballPosition - paddlePosition;
+		paddleDirection.z = 0;
+		// remove z component from vector
+		// normalize vector
+//		paddleDirection.Normalize();
+		// multiply by speed
+		paddleDirection *= speed;
+		// apply to this
+		transform.position += paddleDirection * Time.deltaTime;
+	}
+}
