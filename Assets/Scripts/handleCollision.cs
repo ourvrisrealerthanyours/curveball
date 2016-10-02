c651a135c25fe9ff20d2ed56176acda0ada640d2
Elliot Plant <elliotaplant@gmail.com>
2016-10-02 15:19:18 -0700

    MOre lof of the things

---
+++ Assets/Scripts/handleCollision.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handleCollision : MonoBehaviour {
+
+	public GameObject ball;
+	public GameObject gameManager;
+
+	public enum SideEnum{Player, Enemy};
+	public SideEnum Side;
+
+	private Rigidbody rb;
+	private Collider ballCollider;
+	// Use this for initialization
+	void Start () {
+		ballCollider = ball.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other == ballCollider) {
+			Debug.Log ("Wall hit");
+			gameManager.GetComponent<livesManager> ().decrementLives (Side.ToString());
+			ball.GetComponent<addInitalForce>().reInitialize ();
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/livesManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class livesManager : MonoBehaviour {
+
+	public int enemyLives = 3;
+	public int playerLives = 3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void decrementLives (string side) {
+		if (side == "Enemy") {
+			decreaseEnemyLife ();
+		} else {
+			decreasePlayerLife ();
+		}
+	}
+
+	public void decreasePlayerLife () {
+		Debug.Log ("Player lost life");
+		playerLives--;	
+		if (playerLives == 0) {
+			Debug.Log("you dead, huh");
+		}
+	}
+
+	public void decreaseEnemyLife () {
+		Debug.Log ("Enemy lost life");
+		enemyLives--;
+		if (enemyLives == 0) {
+			Debug.Log("he dead, doe");
+		}
+	}
+}
